6dc3a023fb72a6e183101f4da4d2237e80d51190
Sandun Sahiru <134186922+sandunsahiru@users.noreply.github.com>
2024-06-09 16:45:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  member.style.display = "none";
+  var skills = document.getElementById("skills");
+  skills.style.display = "block";
+  var project = document.getElementById("project");
+  project.style.display = "none";
+}
